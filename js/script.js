569846fe7a53b693d747a9c994f82b29725ebfdd
Alex <AlexLaytmann@gmail.com>
2024-11-01 15:59:40 +0200

    Second Lesson

--- js/script.js
+++ js/script.js
@@ -10,12 +10,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+if(personalMovieDB.count < 10) {
+	console.log('вы просмотрели довольно мало фильмов')
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+	console.log('вы просмотрели много фильмов')
+} else if(personalMovieDB.count >= 30){
+	console.log('Да вы киноман')
+} else{
+	console.log('Ошибка')
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for ( let i = 0; i < 2; i++) {
+	const a = prompt('Один из последних просмотренных фильмов?', ''),
+	b = prompt('На сколько оцените его?', '');
+
+	if(a != '' && b != '' && a != null && b != null && a.length <40 & b < 101){
+		personalMovieDB.movies[a] = b;
+	} else {
+		i--;
+	}
+}
 
 console.log(personalMovieDB);
+
+
